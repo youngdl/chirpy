b9a78966b29a05ba6b179131afd6e2ed4d69790d
Xiaolin Yang <yangxiaolin.sbu@gmail.com>
2021-12-26 21:04:52 -0800

    Create basic template for message board.

--- chirpy/messageboard/views.py
+++ chirpy/messageboard/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from django.template import loader
+
 
 def index(request):
-    return HttpResponse('Hello World')
+    template = loader.get_template('messageboard/index.html')
+    return HttpResponse(template.render({}, request))
