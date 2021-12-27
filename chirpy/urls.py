212108f6d47bca18e8ee74941002e5e99a7619e6
Xiaolin Yang <yangxiaolin.sbu@gmail.com>
2021-12-26 20:41:31 -0800

    Creat messageboar app.

--- chirpy/urls.py
+++ chirpy/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from chirpy import messageboard
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('messageboard/', include('chirpy.messageboard.urls'))
 ]
