d8efd46f1581c61308b0fd9793695b2c43054304
Xiaolin Yang <yangxiaolin.sbu@gmail.com>
2021-12-27 22:44:16 -0800

    Add a placeholder to handle message submit.

--- chirpy/messageboard/urls.py
+++ chirpy/messageboard/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('post_message/', views.post_message, name='post_message'),
 ]
--- chirpy/messageboard/views.py
+++ chirpy/messageboard/views.py
@@ -5,3 +5,9 @@ from django.template import loader
 def index(request):
     template = loader.get_template('messageboard/index.html')
     return HttpResponse(template.render({}, request))
+
+
+def post_message(request):
+    # TODO: check request is get/post
+    # TODO: Write data into db
+    return HttpResponse('Hello World')
