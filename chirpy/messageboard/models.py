baf2d8ec0a0db091cbbf9ad29b7d2a65e477c722
Xiaolin Yang <yangxiaolin.sbu@gmail.com>
2021-12-29 22:04:24 -0800

    Define database and save data into it.

--- chirpy/messageboard/models.py
+++ chirpy/messageboard/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Entry(models.Model):
+    message = models.CharField(max_length=200)
+    user_name = models.CharField(max_length=10)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return f'{self.message}=>{self.user_name}'
--- chirpy/messageboard/views.py
+++ chirpy/messageboard/views.py
@@ -1,15 +1,19 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 from django.http.response import HttpResponseNotAllowed
 from django.template import loader
-
+from chirpy.messageboard import models
+from django.utils import timezone
 
 def index(request):
     template = loader.get_template('messageboard/index.html')
     return HttpResponse(template.render({}, request))
 
 
-def post_message(request):
-    # TODO: Write data into db
+def post_message(request: HttpRequest):
     if request.method != 'POST':
         return HttpResponseNotAllowed(['POST'])
+    post_text = request.POST.get('message')
+    post_user_name = request.POST.get('user_name')
+    entry = models.Entry(message=post_text, user_name=post_user_name, pub_date=timezone.now())
+    entry.save()
     return HttpResponse('Hello World')
